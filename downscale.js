ed3d096373bdd43a0ce092e90347d5c9d97400ee
Florian Lorétan <florian.loretan@wunder.io>
2020-04-23 11:28:43 +0200

    Only downscale releases older than a configurable age.

--- downscale.js
+++ downscale.js
@@ -8,6 +8,9 @@ const k8sExtensionsApi = kc.makeApiClient(k8s.ExtensionsV1beta1Api);
 
 const k8sResourceManager = require('./src/k8sResourceManager');
 
+const defaultMinAge = process.env.DEFAULT_MIN_AGE;
+const releaseMinAge = JSON.parse(process.env.RELEASE_MIN_AGE);
+
 (async function main() {
   try {
     const ingresses = (await k8sExtensionsApi.listIngressForAllNamespaces()).body.items;
@@ -15,9 +18,20 @@ const k8sResourceManager = require('./src/k8sResourceManager');
     ingresses
       .filter(ingress => ingress.metadata.annotations)
       .filter(ingress => ingress.metadata.annotations['auto-downscale/last-update'])
-      // .filter(ingress => moment(ingress.metadata.annotations['auto-downscale/last-update']).add(1, 'hour').isBefore(moment()))
-      .forEach(async ingress => {
+      .filter(ingress => {
+        const lastUpdate = moment(ingress.metadata.annotations['auto-downscale/last-update']);
+        const name = ingress.metadata.name;
+        let minAge = defaultMinAge;
 
+        for (let regex in releaseMinAge) {
+          if (name.match(new RegExp(regex))) {
+            minAge = releaseMinAge[regex];
+          }
+        }
+
+        return lastUpdate.add(...minAge.split(/(\d+)/).filter(match => match)).isBefore(moment());
+      })
+      .forEach(async ingress => {
         const annotations = ingress.metadata.annotations;
         const namespace = ingress.metadata.namespace;
         const serviceName = annotations['auto-downscale/services'];
@@ -28,8 +42,6 @@ const k8sResourceManager = require('./src/k8sResourceManager');
         deployments.forEach(deployment => k8sResourceManager.downscaleResource(deployment, "deployment"));
         cronjobs.forEach(cronjob => k8sResourceManager.downscaleResource(cronjob, "cronjob"));
         statefulsets.forEach(statefulset => k8sResourceManager.downscaleResource(statefulset, "statefulset"));
-
-        console.log(moment(annotations['auto-downscale/last-update']).add(1, 'hour').isBefore(moment()));
       });
   }
   catch (e) {
